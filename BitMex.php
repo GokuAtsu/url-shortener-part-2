c1e76895ecd80138f065f29dcb6ad2a8500866ed
GokuAtsu <106876339+GokuAtsu@users.noreply.github.com>
2020-03-26 08:42:15 +0000

    Readme actualizado

---
+++ BitMex.php
@@ -0,0 +1,29 @@
+
+<?php
+
+class BitMex {
+
+  //const API_URL = 'https://testnet.bitmex.com';
+  const API_URL = 'https://www.bitmex.com';
+  const API_PATH = '/api/v1/';
+  const SYMBOL = 'XBTUSD';
+
+  private $apiKey;
+  private $apiSecret;
+
+  private $ch;
+
+  public $error;
+  public $printErrors = false;
+  public $errorCode;
+  public $errorMessage;
+
+  /*
+   * @param string $apiKey    API Key
+   * @param string $apiSecret API Secret
+   */
+
+  public function __construct($apiKey = '', $apiSecret = '') {
+
+    $this->apiKey = $apiKey;
+    $this->apiSecret = $apiSecret;
