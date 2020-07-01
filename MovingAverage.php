564df3e6142373af688b1d40ed1a23c67488ff2f
GokuAtsu <106876339+GokuAtsu@users.noreply.github.com>
2020-07-01 18:01:24 +0000

    Update Nuget packages

--- MovingAverage.php
+++ MovingAverage.php
@@ -6,3 +6,32 @@ class MovingAverage {
 
   private $PERIODI;
   private $CANDLES;
+
+  public function isPositive(){
+    $resMedia = $this->getLastTwoValues();
+    if((floatval($resMedia[0]) - floatval($resMedia[1])) > 0){
+      return true;
+    }
+    return false;
+  }
+  
+
+  public function getLastValue() {
+    
+    $candele = $this->CANDLES;
+
+    $somma = 0; 
+
+    for($i=0; $i<($this->PERIODI); $i++){
+      $somma += $candele[$i]['close'];
+
+
+    }
+
+    return floatval($somma/$this->PERIODI);
+
+  }
+
+  public function getLastTwoValues() {
+
+    $candele = $this->CANDLES;
