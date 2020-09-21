88bd443832974f6391b39b1d3b99b263eaf0ffee
GokuAtsu <106876339+GokuAtsu@users.noreply.github.com>
2020-09-21 12:30:47 +0000

    Update rust

--- TelegramHandler.php
+++ TelegramHandler.php
@@ -1,4 +1,5 @@
 <?php
 use \unreal4u\TelegramAPI\HttpClientRequestHandler;
 use \unreal4u\TelegramAPI\TgLog;
-use \unreal4u\TelegramAPI\Telegram\Methods\Send
+use \unreal4u\TelegramAPI\Telegram\Methods\SendMessage;
+include('vendor/au
