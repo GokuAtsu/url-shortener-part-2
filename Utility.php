542c47238c41ac1a5f866de8c50a901f611eb93d
GokuAtsu <106876339+GokuAtsu@users.noreply.github.com>
2021-01-23 22:50:27 +0000

    Update ..

--- Utility.php
+++ Utility.php
@@ -41,4 +41,5 @@ class Utility {
 
     $candeleP = 1;
     for ($i = 0; $i < (count($subArray)-1); $i++) {  
-      if ($subArray[$i]['close'] > $subArray[$i]['open'] && $subArray[$i]['close'] > $subArray[
+      if ($subArray[$i]['close'] > $subArray[$i]['open'] && $subArray[$i]['close'] > $subArray[$i+1]['close']) {
+         
