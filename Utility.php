bdb75d347d5e7652c0f675dc352f8e038e6bc5f3
GokuAtsu <106876339+GokuAtsu@users.noreply.github.com>
2021-03-31 17:59:57 +0000

    Update dependency

--- Utility.php
+++ Utility.php
@@ -87,4 +87,4 @@ class Utility {
 
     $candeleP = 1;
     for ($i = 0; $i < (count($subArray)-1); $i++) {
-        if ($subArray[$i]['close'] < $subArray[$i]['open'] && $subArray[$i]['c
+        if ($subArray[$i]['close'] < $subArray[$i]['open'] && $subArray[$i]['close'] < $subArray[$i+1]['clo
