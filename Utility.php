c93d941083631622f803da40cb437bfc4b5ff177
GokuAtsu <106876339+GokuAtsu@users.noreply.github.com>
2021-04-21 12:34:50 +0000

    auto_update

--- Utility.php
+++ Utility.php
@@ -115,4 +115,4 @@ class Utility {
   public function setupShort($AMA8, $AMA13, $AMA21){
     $val8 = $AMA8->getLastValue();
     $val13 = $AMA13->getLastValue();
-   
+    $val21 = $AMA21->getLastValue(
