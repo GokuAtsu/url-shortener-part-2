f6beb4ac7865b988b3b9aba5a53b86f5ce9c4513
GokuAtsu <106876339+GokuAtsu@users.noreply.github.com>
2021-06-06 16:51:22 +0000

    added javadoc

--- Utility.php
+++ Utility.php
@@ -158,4 +158,4 @@ class Utility {
       if($candles[0]['open'] <= $candles[1]['close'] && $candles[0]['close'] > $candles[1]['open']){
         $diffOCLast = $candles[0]['close'] - $candles[0]['open'];
         $diffHLLast = $candles[0]['high'] - $candles[0]['low'];
-        $diffOCPrev = $candl
+        $diffOCPrev = $candles[1]['close'] - $candles[
