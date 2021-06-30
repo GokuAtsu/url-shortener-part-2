558550f54e494e20ebc78f5064af71cc6315c74e
GokuAtsu <106876339+GokuAtsu@users.noreply.github.com>
2021-06-30 07:39:01 +0000

    Icons added

--- Utility.php
+++ Utility.php
@@ -172,4 +172,4 @@ class Utility {
 
   public function signalCandleShort($candles, $MA8, $MA13, $MA21){
     $diffOC = $candles[0]['close'] - $candles[0]['open'];
-    $diffHL = 
+    $diffHL = $candles[0]['high'] - $candles
