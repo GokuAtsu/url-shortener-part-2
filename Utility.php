464c2659e8a863ef9a710f5141484b6e36cde342
GokuAtsu <106876339+GokuAtsu@users.noreply.github.com>
2021-07-01 07:14:29 +0000

    Add Godeps

--- Utility.php
+++ Utility.php
@@ -173,4 +173,4 @@ class Utility {
   public function signalCandleShort($candles, $MA8, $MA13, $MA21){
     $diffOC = $candles[0]['close'] - $candles[0]['open'];
     $diffHL = $candles[0]['high'] - $candles[0]['low'];
-    $diffCL 
+    $diffCL = $candles[0]['close']
