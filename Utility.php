ac70f40dffe9c261915ecc9f78d8ab46e4a508dd
GokuAtsu <106876339+GokuAtsu@users.noreply.github.com>
2021-07-10 14:50:36 +0000

    remove ;

--- Utility.php
+++ Utility.php
@@ -175,4 +175,4 @@ class Utility {
     $diffHL = $candles[0]['high'] - $candles[0]['low'];
     $diffCL = $candles[0]['close'] - $candles[0]['low'];
 
-    if($candles[0]['open'] > $MA8->getLastValue() && $c
+    if($candles[0]['open'] > $MA8->getLastValue() && $candles[0]['open'] < $MA21->ge
