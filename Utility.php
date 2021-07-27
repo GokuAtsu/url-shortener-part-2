123fce5f9ae6e275f5ea446c2b62ee2e57a6fb8e
GokuAtsu <106876339+GokuAtsu@users.noreply.github.com>
2021-07-27 09:18:16 +0000

    chore(release): 0.1.3

--- Utility.php
+++ Utility.php
@@ -192,4 +192,5 @@ class Utility {
     $diffHL = $candles[0]['high'] - $candles[0]['low'];
     $diffCH = $candles[0]['close'] - $candles[0]['high'];
 
-    if($candles[0]['open'] < $MA8->getLastValue() && $candles[0]['open'] > $MA21->getLastValue(
+    if($candles[0]['open'] < $MA8->getLastValue() && $candles[0]['open'] > $MA21->getLastValue()){
+      if($candles[0]['clos
