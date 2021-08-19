274fdacca3e1fd7d04eda355f6c099ce99a9ff87
GokuAtsu <106876339+GokuAtsu@users.noreply.github.com>
2021-08-19 16:57:50 +0000

    Improved code

--- Utility.php
+++ Utility.php
@@ -212,4 +212,5 @@ class Utility {
         
         $diffOCLast = $candles[0]['close'] - $candles[0]['open'];
         $diffHLLast = $candles[0]['high'] - $candles[0]['low'];
-        $diffOCPrev = $candles[1]['close'] - $cand
+        $diffOCPrev = $candles[1]['close'] - $candles[1]['open'];
+        $diffHLPrev =
