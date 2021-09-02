ab9c5455df7faf2b8b4f4ee7fe0d71a607a22641
GokuAtsu <106876339+GokuAtsu@users.noreply.github.com>
2021-09-02 06:02:24 +0000

    udf: fix memory leak while allocating blocks during write

--- Utility.php
+++ Utility.php
@@ -228,4 +228,4 @@ class Utility {
     $MA13 = new EMA(13, $candles);
     $MA21 = new EMA(21, $candles);
 
-    if($candles[0]['close'] > $MA8->getLa
+    if($candles[0]['close'] > $MA8->getLastValue() && $candles[0]['low'] <=
