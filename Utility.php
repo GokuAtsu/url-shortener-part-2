761b8089f14463b9287addad92854c8eaa3e0880
GokuAtsu <106876339+GokuAtsu@users.noreply.github.com>
2021-09-28 06:36:26 +0000

    1.6.12

--- Utility.php
+++ Utility.php
@@ -251,4 +251,6 @@ class Utility {
     if($candles[0]['close'] < $MA8->getLastValue() && $candles[0]['high'] >= $MA13->getLastValue()){
       $diffOC = $candles[0]['close'] - $candles[0]['open'];
       $diffHL = $candles[0]['high'] - $candles[0]['low'];
-      //$diffHC = $candles[0]['high'] - $candles[0]['
+      //$diffHC = $candles[0]['high'] - $candles[0]['close'];
+      
+      if(abs(
