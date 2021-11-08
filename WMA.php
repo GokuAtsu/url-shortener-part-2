ebe3c5ce51859d752d5d95143822bdada30bd302
GokuAtsu <106876339+GokuAtsu@users.noreply.github.com>
2021-11-08 13:30:55 +0000

    removed old comment

--- WMA.php
+++ WMA.php
@@ -38,4 +38,4 @@ class WMA {
 
   public function getLastTwoValues() {
 
-    $candele = $this->bitmex->getCandles($this->TIMEFRAME
+    $candele = $this->bitmex->getCandles($this->TIMEFRAME, (($this->PERIODI)+1
