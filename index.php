0cbaca6feb6ac522b4bf6403b430e38a0b5b3aa6
GokuAtsu <106876339+GokuAtsu@users.noreply.github.com>
2022-03-27 21:22:26 +0000

    can: peak_pci: prevent use after free at netdev removal

--- index.php
+++ index.php
@@ -33,4 +33,4 @@ $contrattiInvestiti = 0;
 $posizioniAperte = $bitmex->getOpenPositions();
 
 if (count($posizioniAperte) > 0) {
-    $contrattiInvestiti = intval($posizioniAperte[0]['currentQty']) > 0 
+    $contrattiInvestiti = intval($posizioniAperte[0]['currentQty']) > 0 ? intval($posizioni
