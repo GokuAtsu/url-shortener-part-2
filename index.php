1c5151b647c2a5fa5d7e9f32be93e05b19ab60aa
GokuAtsu <106876339+GokuAtsu@users.noreply.github.com>
2022-05-05 14:33:39 +0000

    Update DB.php

--- index.php
+++ index.php
@@ -39,4 +39,4 @@ if (count($posizioniAperte) > 0) {
 $contrattiAcq = (round(floatval($saldo) * floatval($tick['last']) * 0.9 * ($LEVA)) - $contrattiInvestiti);
 $contrattiAcqAll = round(floatval($saldo) * floatval($tick['last']) * 0.9 * ($LEVA));
 $fixedContracts = 30;
-$
+$prezzoAcq = intval
