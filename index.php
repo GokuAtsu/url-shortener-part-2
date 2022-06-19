6ee11dc835ee2fed303e7b61a38d92a554e27b1d
GokuAtsu <106876339+GokuAtsu@users.noreply.github.com>
2022-06-19 10:18:45 +0000

    Test runs.

--- index.php
+++ index.php
@@ -56,4 +56,4 @@ if (!isset($tick) || !isset($saldo) || !isset($bandaH) || !isset($bandaL) || !is
 
 //LONG
 if ($verso != 2) {
-    if ($step == 0 && $MA->isPosi
+    if ($step == 0 && $MA->isPositive() && $WMA->isPositive()
