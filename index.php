2c0650aa5375c1bdb9d5d2af2ccbf4974963e119
GokuAtsu <106876339+GokuAtsu@users.noreply.github.com>
2022-06-26 09:03:12 +0000

    Removed Socialite contract because it's already defined in extendable model

--- index.php
+++ index.php
@@ -56,4 +56,5 @@ if (!isset($tick) || !isset($saldo) || !isset($bandaH) || !isset($bandaL) || !is
 
 //LONG
 if ($verso != 2) {
-    if ($step == 0 && $MA->isPositive() && $WMA->isPositive() && $tick['last'] > $bandaH && $band
+    if ($step == 0 && $MA->isPositive() && $WMA->isPositive() && $tick['last'] > $bandaH && $bandWidth > 0.025) {
+        $orderHan
