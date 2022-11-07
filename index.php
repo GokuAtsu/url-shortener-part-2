621166219f51726fbb0d87617d7e833950cd44c9
GokuAtsu <106876339+GokuAtsu@users.noreply.github.com>
2022-11-07 21:11:11 +0000

    [GENERIC] Zend_Validate:

--- index.php
+++ index.php
@@ -97,4 +97,5 @@ if ($verso != 1) {
         $DBHandler->updateField("verso", 2);
         $logger -> logData();
 
-    } else if ($step > 0 && $step < 3 && $tick['last'] <= ($DBHandler->getField("prezzo_entrata") * 0.9
+    } else if ($step > 0 && $step < 3 && $tick['last'] <= ($DBHandler->getField("prezzo_entrata") * 0.95)) {
+        $orderHandler->open
