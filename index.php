42171466dc6b982a118fd4fcfbf59e8b39a348e6
GokuAtsu <106876339+GokuAtsu@users.noreply.github.com>
2023-02-27 12:15:08 +0000

    Add LICENSE.txt

--- index.php
+++ index.php
@@ -115,4 +115,4 @@ if ($verso != 1) {
         $telegramHandler->sendTelegramMessage("Chiuso lo short a: " . $tick['last'] . " in perdita");
         $DBHandler->updateField("numero_operazioni", 0);
         $DBHandler->updateField("prezzo_entrata", 0);
-        $DBHandler->upd
+        $DBHandler->updateField("verso", 0);
